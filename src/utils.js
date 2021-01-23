41b688c13155464fa640cddc4eeced6ebb6b80a4
Dugagjin Ramani <r.dugzino@gmail.com>
2021-01-23 10:03:42 +0100

    update utils added articleAvailability

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,14 @@
+const colors = require('colors');
+
+// Utils
 const urlBuilder = (url, path) => `${url}${path}`;
 
+const articleAvailability = (articleName, url, isAvailable) => {
+  const color = isAvailable ? 'green' : 'red';
+  console.log(`${articleName} -`, `${url}`[color]);
+}
+
 module.exports = {
+  articleAvailability,
   urlBuilder,
 }
