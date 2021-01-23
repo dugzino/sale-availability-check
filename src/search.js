ae0f73de312da1b1a91d894e769c7465ad2414fb
Dugagjin Ramani <r.dugzino@gmail.com>
2021-01-23 08:28:12 +0100

    refactor search.js ease usage

--- src/search.js
+++ src/search.js
@@ -5,16 +5,20 @@ const defaultRetryTimer = 90e3; // = 90 seconds
 
 // STUFF YOU CAN CUSTOMIZE
 const customRetryTimer = null; // Set the value you want
+
 const searchArray = [];
+const searches = [];
 
 // SCRIPT - TO IGNORE
 const searchFn = () => {
   if (!searchArray.length) return console.log(`You didn't setup anything to search yet. Check the README files for more info.`);
 
-  Promise.all([].flatMap(v => v)).then((_) => {
-    console.log();
-    setTimeout(searchFn, customRetryTimer || defaultRetryTimer);
-  });
+  Promise
+    .all(searches.flatMap(v => v))
+    .then((_) => {
+      console.log();
+      setTimeout(searchFn, customRetryTimer || defaultRetryTimer);
+    });
 };
 
 searchFn();
