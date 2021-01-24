93ff31a021ca9ba75028d4d127c8797708deb462
Dugagjin Ramani <r.dugzino@gmail.com>
2021-01-24 01:03:35 +0100

    set customRetryTimer

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,5 @@ const { customRetryTimer } = require('./utils');
 
 module.exports = {
   websiteService: websiteService || null,
-  customRetryTimer: customRetryTimer || null,
+  customRetryTimer,
 }
